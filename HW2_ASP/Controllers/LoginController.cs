4349d6a83dd0e6dcc92d6a814757ef92d39b00f6
daletavg <daletavg@gmail.com>
2019-02-07 12:08:28 +0200

    first commit

---
+++ HW2_ASP/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW2_ASP.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HW2_ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW2_ASP.MvcApplication" Language="C#" %>
